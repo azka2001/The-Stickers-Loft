bc5698a93cb85a8d2e0a0a77cc81ecf152c43359
Azka <75111379+azka2001@users.noreply.github.com>
2023-06-17 10:27:19 +0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,14 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAPnCIT-Gd1fVGN0VE5O4vyEEwSlw6VI2M",
-  authDomain: "thestickersloft.firebaseapp.com",
-  databaseURL: "https://thestickersloft-default-rtdb.firebaseio.com",
-  projectId: "thestickersloft",
-  storageBucket: "thestickersloft.appspot.com",
-  messagingSenderId: "583627301597",
-  appId: "1:583627301597:web:2d0a0dff3887a51acc7134",
-  measurementId: "G-Z3G2FJQX0T"
+ // auth tokens
 };
 
 // Initialize Firebase
